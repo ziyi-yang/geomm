ef3f823e1f863c86242095490fa7324749d13188
Ziyi Yang <ziyi@DN0a1e6f9a.SUNet>
2020-05-31 16:22:47 -0700

    init

--- geomm_results.sh
+++ geomm_results.sh
@@ -1,50 +1,32 @@
-source activate testcross
-
 # Table 1 Results
 EMBEDDINGS="./muse_data/vectors"
 DICTIONARIES="./muse_data/crosslingual/dictionaries"
 echo "Table 1 Results"
 echo "En-Es"
-python geomm.py "$EMBEDDINGS/wiki.en.vec" "$EMBEDDINGS/wiki.es.vec" -dtrain "$DICTIONARIES/en-es.0-5000.txt" -dtest "$DICTIONARIES/en-es.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e3 --max_vocab 200000 --normalize_eval
-echo "Es-En"
-python geomm.py "$EMBEDDINGS/wiki.es.vec" "$EMBEDDINGS/wiki.en.vec" -dtrain "$DICTIONARIES/es-en.0-5000.txt" -dtest "$DICTIONARIES/es-en.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
-echo "En-Fr"
-python geomm.py "$EMBEDDINGS/wiki.en.vec" "$EMBEDDINGS/wiki.fr.vec" -dtrain "$DICTIONARIES/en-fr.0-5000.txt" -dtest "$DICTIONARIES/en-fr.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
-echo "Fr-En"
-python geomm.py "$EMBEDDINGS/wiki.fr.vec" "$EMBEDDINGS/wiki.en.vec" -dtrain "$DICTIONARIES/fr-en.0-5000.txt" -dtest "$DICTIONARIES/fr-en.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e3 --max_vocab 200000 --normalize_eval
-echo "En-De"
-python geomm.py "$EMBEDDINGS/wiki.en.vec" "$EMBEDDINGS/wiki.de.vec" -dtrain "$DICTIONARIES/en-de.0-5000.txt" -dtest "$DICTIONARIES/en-de.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e3 --max_vocab 200000 --normalize_eval
-echo "De-En"
-python geomm.py "$EMBEDDINGS/wiki.de.vec" "$EMBEDDINGS/wiki.en.vec" -dtrain "$DICTIONARIES/de-en.0-5000.txt" -dtest "$DICTIONARIES/de-en.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
-echo "En-Ru"
-python geomm.py "$EMBEDDINGS/wiki.en.vec" "$EMBEDDINGS/wiki.ru.vec" -dtrain "$DICTIONARIES/en-ru.0-5000.txt" -dtest "$DICTIONARIES/en-ru.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e3 --max_vocab 200000 --normalize_eval
-echo "Ru-En"
-python geomm.py "$EMBEDDINGS/wiki.ru.vec" "$EMBEDDINGS/wiki.en.vec" -dtrain "$DICTIONARIES/ru-en.0-5000.txt" -dtest "$DICTIONARIES/ru-en.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
-echo "En-Zh"
-python geomm.py "$EMBEDDINGS/wiki.en.vec" "$EMBEDDINGS/wiki.zh.vec" -dtrain "$DICTIONARIES/en-zh.0-5000.txt" -dtest "$DICTIONARIES/en-zh.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
-echo "Zh-En"
-python geomm.py "$EMBEDDINGS/wiki.zh.vec" "$EMBEDDINGS/wiki.en.vec" -dtrain "$DICTIONARIES/zh-en.0-5000.txt" -dtest "$DICTIONARIES/zh-en.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
-
-# Table 2 Results
-EMBEDDINGS="./vecmap_data/embeddings"
-DICTIONARIES="./vecmap_data/dictionaries"
-echo "Table 2 Results"
-languages=( it de fi es )
-for lang in "${languages[@]}"
-do
-	echo "en-$lang"
-	python geomm.py "$EMBEDDINGS/en.emb.txt" "$EMBEDDINGS/$lang.emb.txt" -dtrain "$DICTIONARIES/en-$lang.train.txt" -dtest "$DICTIONARIES/en-$lang.test.txt" --normalize unit center --max_opt_iter 150 --l2_reg 1e3 --max_vocab 0 --normalize_eval
-done
-
-# Table 6 Results
-EMBEDDINGS="./muse_data/vectors"
-DICTIONARIES="./muse_data/crosslingual/dictionaries"
-echo "Table 6 Results"
-echo "Fr-It-Pt"
-python geomm_cmp_pip.py "$EMBEDDINGS/wiki.fr.vec" "$EMBEDDINGS/wiki.it.vec" "$EMBEDDINGS/wiki.pt.vec" -dtrain1 "$DICTIONARIES/fr-it.excfull.txt" -dtrain2 "$DICTIONARIES/it-pt.excfull.txt" -dtest "$DICTIONARIES/fr-pt.5000-6500.txt"  --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
-echo "It-De-Es"
-python geomm_cmp_pip.py "$EMBEDDINGS/wiki.it.vec" "$EMBEDDINGS/wiki.de.vec" "$EMBEDDINGS/wiki.es.vec" -dtrain1 "$DICTIONARIES/it-de.excfull.txt" -dtrain2 "$DICTIONARIES/de-es.excfull.txt" -dtest "$DICTIONARIES/it-es.5000-6500.txt"  --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
-echo "Es-Pt-Fr"
-python geomm_cmp_pip.py "$EMBEDDINGS/wiki.es.vec" "$EMBEDDINGS/wiki.pt.vec" "$EMBEDDINGS/wiki.fr.vec" -dtrain1 "$DICTIONARIES/es-pt.excfull.txt" -dtrain2 "$DICTIONARIES/pt-fr.excfull.txt" -dtest "$DICTIONARIES/es-fr.5000-6500.txt"  --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
-
-source deactivate
+python geomm.py "$EMBEDDINGS/wiki.en.vec" "$EMBEDDINGS/wiki.vi.vec" -dtrain "$DICTIONARIES/en-vi.0-5000.txt" -dtest "$DICTIONARIES/en-vi.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e3 --max_vocab 200000 --normalize_eval
+# echo "Es-En"
+# python geomm.py "$EMBEDDINGS/wiki.vi.vec" "$EMBEDDINGS/wiki.en.vec" -dtrain "$DICTIONARIES/vi-en.0-5000.txt" -dtest "$DICTIONARIES/vi-en.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
+# echo "En-Th"
+# python geomm.py "$EMBEDDINGS/wiki.en.vec" "$EMBEDDINGS/wiki.th.vec" -dtrain "$DICTIONARIES/en-th.0-5000.txt" -dtest "$DICTIONARIES/en-th.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
+# echo "th-En"
+# python geomm.py "$EMBEDDINGS/wiki.th.vec" "$EMBEDDINGS/wiki.en.vec" -dtrain "$DICTIONARIES/th-en.0-5000.txt" -dtest "$DICTIONARIES/th-en.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e3 --max_vocab 200000 --normalize_eval
+# echo "En-ti"
+# python geomm.py "$EMBEDDINGS/wiki.en.vec" "$EMBEDDINGS/wiki.ti.vec" -dtrain "$DICTIONARIES/en-ti.0-5000.txt" -dtest "$DICTIONARIES/en-ti.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e3 --max_vocab 200000 --normalize_eval
+# echo "ti-En"
+# python geomm.py "$EMBEDDINGS/wiki.ti.vec" "$EMBEDDINGS/wiki.en.vec" -dtrain "$DICTIONARIES/ti-en.0-5000.txt" -dtest "$DICTIONARIES/ti-en.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
+# echo "En-ms"
+# python geomm.py "$EMBEDDINGS/wiki.en.vec" "$EMBEDDINGS/wiki.ms.vec" -dtrain "$DICTIONARIES/en-ms.0-5000.txt" -dtest "$DICTIONARIES/en-ms.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e3 --max_vocab 200000 --normalize_eval
+# echo "ms-En"
+# python geomm.py "$EMBEDDINGS/wiki.ms.vec" "$EMBEDDINGS/wiki.en.vec" -dtrain "$DICTIONARIES/ms-en.0-5000.txt" -dtest "$DICTIONARIES/ms-en.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
+# echo "En-af"
+# python geomm.py "$EMBEDDINGS/wiki.en.vec" "$EMBEDDINGS/wiki.af.vec" -dtrain "$DICTIONARIES/en-af.0-5000.txt" -dtest "$DICTIONARIES/en-af.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
+# echo "af-En"
+# python geomm.py "$EMBEDDINGS/wiki.af.vec" "$EMBEDDINGS/wiki.en.vec" -dtrain "$DICTIONARIES/af-en.0-5000.txt" -dtest "$DICTIONARIES/af-en.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
+# echo "En-bs"
+# python geomm.py "$EMBEDDINGS/wiki.en.vec" "$EMBEDDINGS/wiki.bs.vec" -dtrain "$DICTIONARIES/en-bs.0-5000.txt" -dtest "$DICTIONARIES/en-bs.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
+# echo "bs-En"
+# python geomm.py "$EMBEDDINGS/wiki.bs.vec" "$EMBEDDINGS/wiki.en.vec" -dtrain "$DICTIONARIES/bs-en.0-5000.txt" -dtest "$DICTIONARIES/bs-en.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
+# echo "En-bi"
+# python geomm.py "$EMBEDDINGS/wiki.en.vec" "$EMBEDDINGS/wiki.bi.vec" -dtrain "$DICTIONARIES/en-bi.0-5000.txt" -dtest "$DICTIONARIES/en-bi.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
+# echo "bi-En"
+# python geomm.py "$EMBEDDINGS/wiki.bi.vec" "$EMBEDDINGS/wiki.en.vec" -dtrain "$DICTIONARIES/bi-en.0-5000.txt" -dtest "$DICTIONARIES/bi-en.5000-6500.txt" --normalize unit center  --max_opt_iter 150 --l2_reg 1e2 --max_vocab 200000 --normalize_eval
--- muse_data/get_muse_data.sh
+++ muse_data/get_muse_data.sh
@@ -4,24 +4,21 @@
 
 mkdir -p vectors
 curl -Lo vectors/wiki.en.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.en.vec
-curl -Lo vectors/wiki.es.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.es.vec
-curl -Lo vectors/wiki.fr.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.fr.vec
-curl -Lo vectors/wiki.de.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.de.vec
-curl -Lo vectors/wiki.ru.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.ru.vec
-curl -Lo vectors/wiki.zh.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.zh.vec
-curl -Lo vectors/wiki.it.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.it.vec
-curl -Lo vectors/wiki.pt.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.pt.vec
+curl -Lo vectors/wiki.vi.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.vi.vec
+curl -Lo vectors/wiki.th.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.th.vec
+curl -Lo vectors/wiki.ti.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.ti.vec
+curl -Lo vectors/wiki.ms.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.ms.vec
+curl -Lo vectors/wiki.si.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.si.vec
+curl -Lo vectors/wiki.bs.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.bs.vec
+curl -Lo vectors/wiki.af.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.af.vec
+
 
 aws_path='https://dl.fbaipublicfiles.com/arrival'
-semeval_2017='http://alt.qcri.org/semeval2017/task2/data/uploads'
 
-declare -A wordsim_lg
-wordsim_lg=(["en"]="EN_MC-30.txt EN_MTurk-287.txt EN_RG-65.txt EN_VERB-143.txt EN_WS-353-REL.txt EN_YP-130.txt EN_MEN-TR-3k.txt EN_MTurk-771.txt EN_RW-STANFORD.txt EN_SIMLEX-999.txt EN_WS-353-ALL.txt EN_WS-353-SIM.txt" ["es"]="ES_MC-30.txt ES_RG-65.txt ES_WS-353.txt" ["de"]="DE_GUR350.txt DE_GUR65.txt DE_SIMLEX-999.txt DE_WS-353.txt DE_ZG222.txt" ["fr"]="FR_RG-65.txt" ["it"]="IT_SIMLEX-999.txt IT_WS-353.txt")
-
-mkdir monolingual crosslingual
+mkdir crosslingual
 
 ## Downloading en-{} or {}-en dictionaries
-lgs="de en es fr ru zh"
+lgs="vi th ti ms af bs si"
 mkdir -p crosslingual/dictionaries/
 for lg in ${lgs}
 do
@@ -33,56 +30,3 @@ do
     curl -Lo crosslingual/dictionaries/$fname $aws_path/dictionaries/$fname
   done
 done
-
-## Download European dictionaries
-for src_lg in de es fr it pt
-do
-  for tgt_lg in de es fr it pt
-  do
-    if [ $src_lg != $tgt_lg ]
-    then
-      for suffix in .txt .0-5000.txt .5000-6500.txt
-      do
-        fname=$src_lg-$tgt_lg$suffix
-        curl -Lo crosslingual/dictionaries/$fname $aws_path/dictionaries/european/$fname
-      done
-    fi
-  done
-done
-
-
-## Monolingual wordsim tasks
-for lang in "${!wordsim_lg[@]}"
-do
-  echo $lang
-  mkdir monolingual/$lang
-  for wsim in ${wordsim_lg[$lang]}
-  do
-    echo $wsim
-    curl -Lo monolingual/$lang/$wsim $aws_path/$lang/$wsim
-  done
-done
-
-## SemEval 2017 monolingual and cross-lingual wordsim tasks
-# 1) Task1: monolingual
-curl -Lo semeval2017-task2.zip $semeval_2017/semeval2017-task2.zip
-unzip semeval2017-task2.zip
-
-fdir='SemEval17-Task2/test/subtask1-monolingual'
-for lang in en es de fa it
-do
-  mkdir -p monolingual/$lang
-  uplang=`echo $lang | awk '{print toupper($0)}'`
-  paste $fdir/data/$lang.test.data.txt $fdir/keys/$lang.test.gold.txt > monolingual/$lang/${uplang}_SEMEVAL17.txt
-done
-
-# 2) Task2: cross-lingual
-mkdir -p crosslingual/wordsim
-fdir='SemEval17-Task2/test/subtask2-crosslingual'
-for lg_pair in de-es de-fa de-it en-de en-es en-fa en-it es-fa es-it it-fa
-do
-  echo $lg_pair
-  paste $fdir/data/$lg_pair.test.data.txt $fdir/keys/$lg_pair.test.gold.txt > crosslingual/wordsim/$lg_pair-SEMEVAL17.txt
-done
-rm semeval2017-task2.zip
-rm -r SemEval17-Task2/
